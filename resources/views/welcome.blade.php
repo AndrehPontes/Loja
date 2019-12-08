b7800724f2230df679a15e20d28cd84168d7ae55
André Pontes <contato@andrehost.com.br>
2019-12-08 00:56:40 -0400

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<title>App Crud - Vue.js - Laravel</title>
+<title>App AccessCrédito</title>
 
 <!-- Fonts -->
 <link rel="stylesheet"
